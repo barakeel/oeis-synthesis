15a2790407762f632d6b863d13baba1a3ca245b3
Thibault Gauthier <email@thibaultgauthier.fr>
2022-05-17 08:24:39 +0200

    more about removing openblas and mkl parallelization

--- tnn_in_c/compile_ob.sh
+++
@@ -1,7 +0,0 @@
-export LD_LIBRARY_PATH=/home/thibault/big/OpenBLAS:$LD_LIBRARY_PATH
-export OPENBLAS_NUM_THREADS=2
-rm ob.o
-rm ob.so
-cat ob_fst.c ob_arity ob_head ob_mat ob_snd.c > ob.c
-gcc -fPIC -c ob.c -o ob.o -I/home/thibault/big/OpenBLAS
-gcc -shared -o ob.so ob.o /usr/lib/x86_64-linux-gnu/libm.a /home/thibault/big/OpenBLAS/libopenblas.a
