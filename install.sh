25e4e012d3dcd9a9dce36a266d0ccbd758f66df3
Thibault Gauthier <email@thibaultgauthier.fr>
2022-06-13 20:38:31 +0200

    updating install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,14 @@ sed "s#directory_template#$PWD#g" tnn_in_c/tree_template > tnn_in_c/tree_temp
 sed "s#dimension_template#$DIM#g" tnn_in_c/tree_temp > tnn_in_c/tree.c
 rm tnn_in_c/tree_temp
 
+if [ -f "tnn_in_c/compile_ob.sh" ]
+then
+echo 'Keeping existing "tnn_in_c/compile_ob.sh" file'
+else
+   echo 'File "tnn_in_c/compile_ob.sh" not found: copying it from "tnn_in_c/compile_ob_thibault.sh"'; 
+   cp tnn_in_c/compile_ob_thibault.sh tnn_in_c/compile_ob.sh
+fi
+
 Holmake cleanAll
 Holmake
 
