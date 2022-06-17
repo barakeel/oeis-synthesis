19836f67b74366012796a60bf5d2abc9acf6cf00
Thibault Gauthier <email@thibaultgauthier.fr>
2022-06-17 17:17:34 +0200

    better install message

--- install.sh
+++ install.sh
@@ -1,25 +1,28 @@
 
 if [ -f "config" ]
 then
-echo 'Proceeding with parameters from "config"'
+echo 'Keep config'
 else
-   echo 'File "config" not found: copying it from "config_template"'; 
+   echo 'Create config from config_template'; 
    cp config_template config 
 fi
 
 sed "s#directory_template#$PWD#g" dir_template > dir.sml
 DIM=$(grep '^dim_glob' config_template | sed -e 's/dim_glob *//')
+
+echo 'Overwrite tree.c with tree_template and config parameters'
 sed "s#directory_template#$PWD/tnn_in_c#g" tnn_in_c/tree_template > tnn_in_c/tree_temp
 sed "s#dimension_template#$DIM#g" tnn_in_c/tree_temp > tnn_in_c/tree.c
 rm tnn_in_c/tree_temp
 
+echo 'Overwrite tnn_in_c/ob_fst.c with tnn_in_c/ob_fst_template and config parameters'
 sed "s#dimension_template#$DIM#g" tnn_in_c/ob_fst_template > tnn_in_c/ob_fst.c
 
 if [ -f "tnn_in_c/compile_ob.sh" ]
 then
-echo 'Keeping existing "tnn_in_c/compile_ob.sh" file'
+echo 'Keep tnn_in_c/compile_ob.sh'
 else
-   echo 'File "tnn_in_c/compile_ob.sh" not found: copying it from "tnn_in_c/compile_ob_thibault.sh"'; 
+   echo 'Create tnn_in_c/compile_ob.sh from tnn_in_c/compile_ob_thibault.sh'; 
    cp tnn_in_c/compile_ob_thibault.sh tnn_in_c/compile_ob.sh
 fi
 
