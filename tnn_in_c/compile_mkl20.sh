bd8267039782662bc3b13971e7ecbe5399a6fd07
Thibault Gauthier <email@thibaultgauthier.fr>
2022-07-01 15:35:26 +0200

    no jit

--- tnn_in_c/compile_mkl20.sh
+++ tnn_in_c/compile_mkl20.sh
@@ -1,1 +1,1 @@
-gcc -o tree tree.c -DMKL_ILP64 -m64 -I/usr/include/mkl -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl -DMKL_DIRECT_CALL_JIT
+gcc -o tree tree.c -DMKL_ILP64 -m64 -I/usr/include/mkl -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl
