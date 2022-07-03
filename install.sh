4f0fb9d1911ecbc4ec36903d7953ac088519967a
Thibault Gauthier <email@thibaultgauthier.fr>
2022-07-04 00:00:21 +0200

    remove directory template

--- install.sh
+++ install.sh
@@ -11,10 +11,7 @@ sed "s#directory_template#$PWD#g" dir_template > dir.sml
 DIM=$(grep '^dim_glob' config_template | sed -e 's/dim_glob *//')
 
 echo 'Overwrite tree.c'
-sed "s#directory_template#$PWD/tnn_in_c#g" tnn_in_c/tree_template > tnn_in_c/tree_temp
-sed "s#dimension_template#$DIM#g" tnn_in_c/tree_temp > tnn_in_c/tree.c
-rm tnn_in_c/tree_temp
-
+sed "s#dimension_template#$DIM#g" tnn_in_c/tree_template > tnn_in_c/tree.c
 echo 'Overwrite tnn_in_c/ob_fst.c'
 sed "s#dimension_template#$DIM#g" tnn_in_c/ob_fst_template > tnn_in_c/ob_fst.c
 
