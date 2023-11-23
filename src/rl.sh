2421960bcae387fddbdba8a9abd3f9bd1f72af56
Thibault Gauthier <email@thibaultgauthier.fr>
2023-11-23 18:57:16 +0100

    safer setting of environment variables

--- src/rl.sh
+++ src/rl.sh
@@ -1,4 +1,6 @@
 export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 EXP=$1
 sed "s#expname_template#$EXP#g" search_script_template > search_script
