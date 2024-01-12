0858fba4efbf60c847dd843bf786369a66053c29
Thibault Gauthier <email@thibaultgauthier.fr>
2024-01-12 18:12:16 +0100

    update recheck

--- src/recheck.sh
+++ src/recheck.sh
@@ -1,6 +1,6 @@
 for i in {80..99}
 do 
 j=$((i-1)) 
-cp "exp4$j-chk/solnew" "exp4$i-chk/solold"
-cp "exp4$i-chk/solold" "exp4$i-chk/solnew" 
+cp "exp/exp4$j-chk/solnew" "exp/exp4$i-chk/solold"
+cp "exp/exp4$i-chk/solold" "exp/exp4$i-chk/solnew" 
 done
