c3031e10c07610fe60f0a708251cf68ad7301ee7
Thibault Gauthier <email@thibaultgauthier.fr>
2024-02-03 15:40:46 +0100

    recheck script

---
+++ src/recheck_air03.sh
@@ -0,0 +1,10 @@
+mkdir exp/nmt$1
+cd exp/nmt$1
+scp 10.35.125.70:/home/mptp/big3/oe3-run1-op1/big-merge/out1/00.z$1.gz z$1.gz
+gunzip -o candorg z$1.gz
+awk -F':' '{print $3}' candorg | sed 's/\(.\)/\1 /g; s/ $//' > cand
+cd ../..
+sh check.sh nmt$1
+cd exp/nmt$1
+rm z$1.gz candorg cand
+rm -r split
