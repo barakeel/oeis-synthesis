3865b318e1bc2325bb0aa186a36e7191796b873d
Thibault Gauthier <email@thibaultgauthier.fr>
2024-02-03 16:12:30 +0100

    creating solold file

--- src/recheck_air03.sh
+++ src/recheck_air03.sh
@@ -3,10 +3,19 @@ mkdir exp/nmt$1
 cd exp/nmt$1
 scp 10.35.125.70:/home/mptp/big3/oe3-run1-op1/big-merge/out1/00.z$1.gz z$1.gz
 gunzip z$1.gz
+echo "adding spaces"
 awk -F':' '{gsub(/./, "& ", $3); print $3}' z$1 > cand
+echo "()" > solold
 cd $DIR
+echo "start checking"
 sh check.sh nmt$1
-#cd exp/nmt$1
-#rm z$1.gz z$1 cand
-#rm -r split
-#cd ../..
+echo "end checking"
+cd exp/nmt$1
+if [ -f "solnew" ]
+rm z$1.gz z$1 cand
+rm -r split
+else
+  echo ""
+  exit 1
+fi
+cd $DIR
