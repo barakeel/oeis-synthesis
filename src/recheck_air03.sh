fd68483c600751c6ce85382a5b7380766895e7f1
Thibault Gauthier <email@thibaultgauthier.fr>
2024-02-03 16:05:40 +0100

    commenting out

--- src/recheck_air03.sh
+++ src/recheck_air03.sh
@@ -1,11 +1,12 @@
+DIR=$PWD
 mkdir exp/nmt$1
 cd exp/nmt$1
 scp 10.35.125.70:/home/mptp/big3/oe3-run1-op1/big-merge/out1/00.z$1.gz z$1.gz
 gunzip z$1.gz
 awk -F':' '{gsub(/./, "& ", $3); print $3}' z$1 > cand
-cd ../..
+cd $DIR
 sh check.sh nmt$1
-cd exp/nmt$1
-rm z$1.gz z$1 cand
-rm -r split
-cd ../..
+#cd exp/nmt$1
+#rm z$1.gz z$1 cand
+#rm -r split
+#cd ../..
