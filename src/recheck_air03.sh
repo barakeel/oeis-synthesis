8dd8b738d2dfd465065d822f805fa6dc5c4b8052
Thibault Gauthier <email@thibaultgauthier.fr>
2024-02-03 15:56:11 +0100

    single awk command

--- src/recheck_air03.sh
+++ src/recheck_air03.sh
@@ -2,7 +2,7 @@ mkdir exp/nmt$1
 cd exp/nmt$1
 scp 10.35.125.70:/home/mptp/big3/oe3-run1-op1/big-merge/out1/00.z$1.gz z$1.gz
 gunzip z$1.gz
-awk -F':' '{print $3}' z$1 | sed 's/\(.\)/\1 /g; s/ $//' > cand
+awk -F':' '{gsub(/./, "& ", $3); print $3}' z$1 > cand
 cd ../..
 sh check.sh nmt$1
 cd exp/nmt$1
